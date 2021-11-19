30cc1eed040e0e0b34fd94ac1b86d498143a2688
souheilbenslama <benslamasouheil@gmail.com>
2021-11-19 16:54:03 +0100

    adding basic pet  crud services

--- src/pet/pet.controller.ts
+++ src/pet/pet.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, HttpStatus, Post, Res, UseFilters } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post, Res, UseFilters } from '@nestjs/common';
 import { response } from 'express';
+import { ObjectId } from 'mongoose';
 import { addPetDto } from './dto/addpet.dto';
+import { updatePetDto } from './dto/updatepet.dto';
 import { PetAddFilter } from './pet-add.filter';
 import { Pet } from './pet.schema';
 import { PetService } from './pet.service';
@@ -8,13 +10,48 @@ import { PetService } from './pet.service';
 @Controller('pets')
 export class PetController {
     constructor(private readonly petService: PetService){}
+    
+    
     @Post()
     @UseFilters( new PetAddFilter())
     async addPet(@Res() response,@Body() pet:addPetDto){
        
-        const newPet = await this.petService.create(pet) ;
-        return response.status(HttpStatus.CREATED).json({newPet}) ; 
+        const newPet = await this.petService.createPet(pet) ;
+        return response.status(HttpStatus.CREATED).json(newPet) ; 
         
     }
 
+ 
+    @Get()
+    async getPets(@Res() response){
+        const pets= await this.petService.findAllPets() ;
+        return response.status(HttpStatus.OK).json(pets)
+
+    }
+
+    @Get("/one/:id")
+    async getPetById(@Res()response , @Param("id") id : string ){
+        const pets= await this.petService.findByPetId(id) ;
+        return response.status(HttpStatus.OK).json(pets) ;
+    }
+
+    @Get("/:uid")
+    async getUserPets(@Res()response , @Param("uid") id :string){
+        const pets= await this.petService.findPetByUserId(id) ;
+        return response.status(HttpStatus.OK).json(pets) ;
+
+    }
+
+    @Patch("/:id")
+    async updatePet(@Res() response , @Param("id") id:string , @Body() update: updatePetDto){
+        const result = await this.petService.updatePet(id,update) ;
+        return response.status(HttpStatus.OK).json(result) ;
+    }
+
+    @Delete("/:id")
+    async deletePet( @Res() response , @Param("id") id : string ) {
+        const result = await this.petService.deletePet(id) ; 
+        return response.status(HttpStatus.OK).json(result) ; 
+    }
+  
 }
--- src/pet/pet.module.ts
+++ src/pet/pet.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PetController } from './pet.controller';
 import { Pet, PetSchema } from './pet.schema'
 import { PetService } from './pet.service';
+import { Weight, WeightSchema } from './weight.schema';
+//import { WeightModule } from './weight/weight.module';
 
 @Module({
   controllers: [ PetController],imports:[
 
-    MongooseModule.forFeature([{name: Pet.name, schema: PetSchema}])], providers: [PetService],
+    MongooseModule.forFeature([{name: Pet.name, schema: PetSchema},{name: Weight.name, schema: WeightSchema}]),
+
+    ], providers: [PetService],
 })
 export class PetModule {}
