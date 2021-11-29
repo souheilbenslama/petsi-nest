19544dc718993bb061e8b62624f3f280bee5db51
Marouen-Mahou <mahou.marouen@gmail.com>
2021-11-29 11:43:50 +0100

    fixed

--- src/vaccine/vaccine.controller.ts
+++ src/vaccine/vaccine.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post, Res, UseFilters } from '@nestjs/common';
+import { Body, Controller, Put,Get, HttpStatus, Param, Post, Res, UseFilters, Delete } from '@nestjs/common';
 import { response } from 'express';
-import { ObjectId } from 'mongoose';
-import { AddVaccineDto } from './dto/addVaccine.dto';
-import { UpdateVaccineDto } from './dto/updateVaccine.dto';
+import { AddVaccineDto } from './dto/addvaccine.dto';
+import { UpdateVaccineDto} from './dto/updateVaccine.dto'
 import { VaccineAddFilter } from './vaccine-add.filter';
 import { Vaccine } from './vaccine.schema';
 import { VaccineService } from './vaccine.service';
@@ -10,42 +9,46 @@ import { VaccineService } from './vaccine.service';
 @Controller('vaccines')
 export class VaccineController {
     constructor(private readonly vaccineService: VaccineService){}
+
+
     @Post()
     @UseFilters( new VaccineAddFilter())
     async addVaccine(@Res() response,@Body() vaccine:AddVaccineDto){
+       try{
         const newVaccine = await this.vaccineService.create(vaccine) ;
-        return response.status(HttpStatus.CREATED).json(newVaccine) ;    
+        return response.status(HttpStatus.CREATED).json(newVaccine) ;
+       }catch(e){
+        return response.status(HttpStatus.BAD_REQUEST).send(e) ;
+       }
+         
+        
     }
 
- 
-    @Get()
-    async getVaccines(@Res() response){
-        const vaccines= await this.vaccineService.find({deleted:false}) ;
-        return response.status(HttpStatus.OK).json(vaccines)
+    @Get(':id')
+    async getVaccine(@Res() response,@Param() param){
+        try {
+            const vaccine = await this.vaccineService.findOne({ _id : param.id})
+            return response.status(HttpStatus.OK).json(vaccine) ;
+        } catch(e) {
+            return response.status(HttpStatus.BAD_REQUEST).send(e) ;
+        }
     }
 
-    @Get("/one/:id")
-    async getVaccineById(@Res()response , @Param("id") id : string ){
-        const vaccines= await this.vaccineService.find({_id:id,deleted:false}) ;
+    @Get('')
+    async getAllVaccine(@Res() response){
+        const vaccines = await this.vaccineService.find({})
         return response.status(HttpStatus.OK).json(vaccines) ;
     }
 
-    @Get("/:uid")
-    async getUserVaccines(@Res()response , @Param("uid") id :string){
-        const vaccines= await this.vaccineService.find({"owner":id,deleted:false}) ;
-        return response.status(HttpStatus.OK).json(vaccines) ;
+    @Put(':id')
+    async updateVaccine(@Res() response,@Body() vaccine:UpdateVaccineDto, @Param() param){
+        const updatedVaccine = await this.vaccineService.findOneAndUpdate({_id: param.id}, vaccine)
+        return response.status(HttpStatus.OK).json(updatedVaccine) ;
     }
 
-    @Patch("/:id")
-    async updateVaccine(@Res() response , @Param("id") id:string , @Body() update: UpdateVaccineDto){
-        const result = await this.vaccineService.findOneAndUpdate({_id:id,deleted:false},{$set:update},{new:true}) ;
-        return response.status(HttpStatus.OK).json(result) ;
-    }
-
-    @Delete("/:id")
-    async deleteVaccine( @Res() response , @Param("id") id : string ) {
-        const result = await this.vaccineService.findOneAndUpdate({_id:id},{$set:{deleted:true,deleted_At:Date.now()}},{new:true}) ; 
+    @Delete(':id')
+    async deleteVaccine(@Res() response,@Param("id") id : string){
+        const result = await this.vaccineService.findOneAndUpdate({_id:id},{$set:{deleted:true,deleted_At:Date.now()}},{new:true}); 
         return response.status(HttpStatus.OK).json(result) ; 
     }
-  
 }
