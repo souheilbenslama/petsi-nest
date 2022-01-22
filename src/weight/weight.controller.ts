61f1dd19871601915991cf6d3e2807b7a6f2e9bf
souheilbenslama <benslamasouheil@gmail.com>
2022-01-22 11:30:18 +0100

    remove pet submodule weight and chage dependencies for heroku deploy 8

--- src/weight/weight.controller.ts
+++ src/weight/weight.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Put,Get, HttpStatus, Param, Post, Res, UseFilters, Delete } from '@nestjs/common';
-import { response } from 'express';
+
 import { AddWeightDto } from 'src/weight/dto/add-weight.dto';
-import { UpdateWeightDto } from 'src/weight/dto/updateWeight.dto'
+
+import { UpdateWeightDto } from 'src/weight/dto/upweight.dto';
 
 import { WeightService } from 'src/weight/weight.service';
 
