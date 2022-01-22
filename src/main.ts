f50e9c80ed4a92e8451fe99600c6550e413b5744
souheilbenslama <benslamasouheil@gmail.com>
2022-01-22 11:42:06 +0100

    remove pet submodule weight and chage dependencies for heroku deploy 10

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   app.use(morgan('dev')) ; 
   app.use(helmet()) ;
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
